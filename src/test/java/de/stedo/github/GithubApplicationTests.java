0dce5cc9e236a5f3642a723720da018bd93a0438
Steve Dölgner <steve.doelgner@gmail.com>
2022-02-15 11:51:40 +0100

    fail test

--- src/test/java/de/stedo/github/GithubApplicationTests.java
+++ src/test/java/de/stedo/github/GithubApplicationTests.java
@@ -1,5 +1,6 @@
 package de.stedo.github;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class GithubApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.fail();
     }
 
 }
